e4dafe6a60f2dfdd7b1ea27a001c5c4ae8e86ce2
Konrad Bobowski <konrad.bobowski92@gmail.com>
2018-11-22 23:45:05 +0100

    "login template"

---
+++ src/main/java/pl/konrad/feak_news/controller/UserController.java
@@ -0,0 +1,7 @@
+package pl.konrad.feak_news.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+}
---
+++ src/main/java/pl/konrad/feak_news/model/forms/UserForm.java
@@ -0,0 +1,9 @@
+package pl.konrad.feak_news.model.forms;
+
+import lombok.Data;
+
+@Data
+public class UserForm {
+    private String login;
+    private String password;
+}
