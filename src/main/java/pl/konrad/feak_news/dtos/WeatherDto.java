edf43b043e0875a7ab956b44551e361173e06013
DieselPowerX <konrad.bobowski92@gmail.com>
2018-12-04 15:46:45 +0100

    "finished adding weather api"

--- src/main/java/pl/konrad/feak_news/dtos/WeatherDto.java
+++ src/main/java/pl/konrad/feak_news/dtos/WeatherDto.java
@@ -3,6 +3,8 @@ package pl.konrad.feak_news.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class WeatherDto {
 
@@ -15,22 +17,41 @@ public class WeatherDto {
     @JsonProperty("id")
     private int id;
 
+    @JsonProperty("weather")
+    private List<ConditionDto> conditionDto;
+
+
     @JsonProperty("name")
     private String name;
 
     public static class TempDto {
         @JsonProperty("temp")
         private double temperature;
-        public double getTemperature() {
-            return temperature - 273.15;
+        public String getTemperature() {
+            return String.valueOf(Math.round(temperature - 273.15)) + "\u00b0C";
         }
     }
 
     public static class CloudsDto{
         @JsonProperty("all")
         private int clouds;
-        public int getClouds(){
-            return clouds;
+
+        public String getClouds(){
+            if(clouds <= 10){
+                return"noClouds";
+            }else if (clouds <=50){
+                return"someClouds";
+            }else{
+                return"clouds";
+            }
+        }
+    }
+
+    public static class ConditionDto{
+        @JsonProperty("main")
+        private String state;
+        public String getState(){
+            return state;
         }
     }
 }
